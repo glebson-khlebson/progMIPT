8809dff1edb66b2ff2fe539e25c11d9d3e9884e2
glebson-khlebson <60920668+glebson-khlebson@users.noreply.github.com>
2022-03-28 20:50:59 +0300

    Update hyper.py

--- hyper.py
+++ hyper.py
@@ -1,83 +1,11 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "with open('SISSO_predict_para', 'r') as file:\n",
-    "    STR = file.read()\n",
-    "    file.close()\n",
-    "nsample = str(45 - int(STR[0]))\n",
-    "with open('SISSO.in', 'r') as file_read:\n",
-    "    s  = file_read.read()\n",
-    "    s = s.replace('nsample=45', 'nsample='+nsample)\n",
-    "    file_read.close()\n",
-    "with open('SISSO.in', 'w') as file_write:\n",
-    "    file_write.write(s)\n",
-    "    file_write.close()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'42'"
-      ]
-     },
-     "execution_count": 6,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+with open('SISSO_predict_para', 'r') as file:
+    STR = file.read()
+    file.close()
+nsample = str(45 - int(STR[0]))
+with open('SISSO.in', 'r') as file_read:
+    s  = file_read.read()
+    s = s.replace('nsample=45', 'nsample='+nsample)
+    file_read.close()
+with open('SISSO.in', 'w') as file_write:
+    file_write.write(s)
+    file_write.close()
